c254a1cc7965f57a36fc33ebf6073e5e66dbbdae
Stefano Foti <32981494+stefanofoti@users.noreply.github.com>
2021-04-26 13:26:56 +0200

    bugfix

--- helpers/rabbitmq.js
+++ helpers/rabbitmq.js
@@ -1,42 +1,100 @@
 "use strict";
 
-var amqp = require('amqplib/callback_api')
 var config = require('../config.json')
+var open = require('amqplib').connect(config.AMQP_URI);
 
-let ch = null;
-let ch2 = null;
+//var amqp = require('amqplib/callback_api')
+//let ch = null;
+//let ch2 = null;
 
+/*
 amqp.connect(config.AMQP_URI, function (err, conn) {
     conn.createChannel(function (err, channel) {
-       ch = channel
+        ch = channel
     })
 })
 amqp.connect(config.AMQP_URI, function (err, conn) {
     conn.createChannel(function (err, channel) {
-       ch2 = channel
+        ch2 = channel
     })
 })
+*/
 
 module.exports = (fastify, opts) => {
 
+    function publish(q, msg) {
+        let ch;
+        var connection;
+        let publisher = open.then(function(conn) {
+            connection=conn;
+            return conn.createChannel();
+        }).then(function(chann) {
+            ch = chann;
+            const queue = ch.assertQueue(q, {
+                exclusive: false,
+                durable: false,
+                autoDelete: true
+            })
+            console.log(queue)
+            ch.purgeQueue(q).catch(function(err) {});
+            return queue
+        }).then(function(ok) {
+            return ch.sendToQueue(q, Buffer.from(msg),{noAck:true});
+        }).then(function(ok) {
+            console.log('MESSAGE_SENT', ok);
+            return ok; //resolve('MESSAGE_SENT')
+        })
+        /*.then((ok)=>{
+            console.log('Assert queue response:', ok);
+            setTimeout(function() { connection.close();}, 500);
+        })*/
+        .catch(function(err) {
+            console.log(err);
+            console.error('Unable to connect PUBLISHER');
+            // process.exit(1);
+            //reject('MESSAGE_SENT')
+        });
+    
+        return publisher
+    }
+
     const sendMessage = async (queueNames, data) => {
-        console.log("sending message "+queueNames)
+        for (let index = 0; index < queueNames.length; index++) {
+            const q = queueNames[index]
+            const msg = data[index]
+            publish(q, msg)
+        }
+        /*
+        console.log("sending message " + queueNames)
         ch.assertQueue(queueNames[0], {
             exclusive: false,
             durable: false,
             autoDelete: true
         })
-        if(queueNames.length == 2) {
+        ch.purgeQueue(queueNames[0], function callback(err, result) {
+            console.log(result)
+            if (err) {
+                console.log("No messages purged.")
+            }
+        })
+        if (queueNames.length == 2) {
             ch2.assertQueue(queueNames[1], {
                 exclusive: false,
                 durable: false,
                 autoDelete: true
-            })    
+            })
+            ch2.purgeQueue(queueNames[1], function callback(err, result) {
+                console.log(result)
+                if (err) {
+                    console.log("No messages purged.")
+                }
+            })
         }
         ch.sendToQueue(queueNames[0], Buffer.from(data[0]))
-        if(queueNames.length == 2) {
+        if (queueNames.length == 2) {
             ch2.sendToQueue(queueNames[1], Buffer.from(data[1]))
-        }
+        }*/
+
     }
 
     return {
